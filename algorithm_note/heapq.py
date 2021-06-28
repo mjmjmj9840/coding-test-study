be80aaa1db1bc49dfd514074c14d9e24ce8427ba
Kim Mi Jin <mjmjmj9840@gmail.com>
2021-06-28 22:29:35 +0900

    Create heapq.py

---
+++ algorithm_note/heapq.py
@@ -0,0 +1,43 @@
+'''heapq 기본 사용법'''
+heapq.heappush(heap, x) # heap에 x 삽입
+heapq.heappop(heap) # heap에서 원소 꺼내기
+
+
+
+
+'''heapsort'''
+import heapq
+
+def heapsort(iterable):
+  h = []
+  result = []
+  # 모든 원소를 차례대로 힙에 삽입
+  for value in iterable:
+    heapq.heappush(h, value)
+  # 힙에 삽입된 모든 원소를 차례대로 꺼내기
+  for _ in range(len(h)):
+    result.append(heapq.heappop(h))
+  return result
+
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+ 
+ 
+ 
+'''최대 힙을 이용한 내림차순 heapsort'''
+import heapq
+
+def heapsort(iterable):
+  h = []
+  result = []
+  # 모든 원소를 차례대로 힙에 삽입
+  for value in iterable:
+    heapq.heappush(h, -value)
+  # 힙에 삽입된 모든 원소를 차례대로 꺼내기
+  for _ in range(len(h)):
+    result.append(-heapq.heappop(h))
+  return result
+
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result) # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
