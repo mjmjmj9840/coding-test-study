af57bace2d57930cf82fcbacbb42d722864085e3
Kim Mi Jin <mjmjmj9840@gmail.com>
2021-07-07 13:57:37 +0900

    Update sort.py

--- algorithm_note/sort.py
+++ algorithm_note/sort.py
@@ -11,3 +11,21 @@ def sel_sort(array):
   return array
 
 # 시간복잡도: O(N^2)
+
+
+
+
+''' 삽입 정렬 '''
+
+def ins_sort(array):
+  for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+      if array[j] < array[j - 1]: # 한 칸씩 왼쪽으로 이동
+        array[j], array[j - 1] = array[j - 1], array[j]
+      else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+        break
+
+  return array
+
+# 시간복잡도: O(N^2)
+# 최선의 경우: O(N)
