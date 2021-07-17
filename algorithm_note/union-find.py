ce9d1ad3084f5f376dedaa35e788dc46370142be
Kim Mi Jin <mjmjmj9840@gmail.com>
2021-07-17 14:33:46 +0900

    Create union-find.py

---
+++ algorithm_note/union-find.py
@@ -0,0 +1,74 @@
+'''
+union-find data structure
+'''
+
+
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1) # 부모 테이블 초기화
+
+# 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+
+
+
+# 사용예시1
+
+# Union 연산을 각각 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력하기
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+# 부모 테이블 내용 출력하기
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
+
+
+
+
+# 사용예시2 (사이클 판별)
+
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(Union) 연산 수행
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
