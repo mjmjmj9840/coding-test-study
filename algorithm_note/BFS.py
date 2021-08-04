88842752b3a8854f89330a5b536a0c3a1351decf
Kim Mi Jin <mjmjmj9840@gmail.com>
2021-08-04 10:32:00 +0900

    BFS 사용 예시와 시간 복잡도 추가

--- algorithm_note/BFS.py
+++ algorithm_note/BFS.py
@@ -12,3 +12,25 @@ def bfs(graph, start, visited):
       if not visited[i]:
         queue.append(i)
         visited[i] = True
+
+        
+        
+        
+''' 사용 예시 '''
+
+# 노드의 개수, 간선의 개수, 탐색 시작 지점
+v, e, start = map(int, input().split())
+
+graph = [[] for _ in range(v + 1)]
+visited = [False] * (v + 1)
+
+# 간선 정보 입력 받기(양방향 그래프 가정)
+for _ in range(e):
+  a, b = map(int, input())
+  graph[a].append(b)
+  graph[b].append(a)
+  
+bfs(graph, start, visited)
+
+
+# 시간 복잡도: O(V + E)
